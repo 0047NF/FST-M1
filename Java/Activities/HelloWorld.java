c674b758604b7ff7066dae9c41d557147c7b0ae4
0047NF <134353838+0047NF@users.noreply.github.com>
2024-12-18 22:58:42 +0530

    Add files via upload

---
+++ Java/Activities/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String name= "Kumar";
+		int a =10;
+		int b =30;
+		System.out.println(name);
+		System.out.println(a < b);
+		System.out.println("Hello World..!!");
+	}
+
+}
