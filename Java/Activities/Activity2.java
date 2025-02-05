5362c8ab3b1be24d6e248c6358490580ca26f81a
0047NF <134353838+0047NF@users.noreply.github.com>
2025-02-05 17:03:01 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+/*public class Activity2 {
+
+	//public static void main(String[] args)
+	//{
+		// TODO Auto-generated method stub
+		//Initialize the array
+        //int[] numArr = {10, 77, 10, 54, -11, 10};
+      //   System.out.println("Original Array: " + Arrays.toString(numArr));       
+    
+	//}
+	
+	
+	
+	
+	
+}*/
+
+public class Activity2 
+{
+    public static void main(String[] args)
+    {
+        int[] Arr = {10, 77, 10, 54, -11, 10};
+        
+        System.out.println("Original Array: " + Arrays.toString(Arr));
+        
+        int searchNum = 10;
+        int fixedSum = 30;
+
+        System.out.println("Result: " + result(Arr, searchNum, fixedSum));
+    }
+
+    public static boolean result(int[] numbers, int searchNum, int fixedSum)
+    {
+    	
+        int temp_sum = 0;
+        
+        for (int number : numbers) {
+            if (number == searchNum) {
+                temp_sum += searchNum;
+            }
+
+            if (temp_sum > fixedSum) {
+                break;
+            }
+        }
+
+        return temp_sum == fixedSum;
+    }
+}
