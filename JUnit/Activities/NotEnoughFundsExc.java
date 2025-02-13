e35aa31d41fc2e3fe69dfb88cae94de69587c8d4
0047NF <134353838+0047NF@users.noreply.github.com>
2025-02-13 20:45:19 +0530

    Delete JUnit/Activities/NotEnoughFundsExc.java

--- JUnit/Activities/NotEnoughFundsExc.java
+++
@@ -1,9 +0,0 @@
-package activities;
-
-public class NotEnoughFundsExc extends RuntimeException {
-
-    public NotEnoughFundsException(Integer amount, Integer balance) {
-        super("Attempted to withdraw " + amount + " with a balance of " + balance);
-    }
-
-}
